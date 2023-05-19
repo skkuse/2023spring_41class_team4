acce3d491cbf67a9b4cf621d744f2c259b652f77
SeongHyeon Cho <67233988+st42597@users.noreply.github.com>
2023-05-19 18:09:36 +0900

    feat: add instructor page nav

--- client/src/app/study/instructor/page.js
+++ client/src/app/study/instructor/page.js
@@ -1,7 +1,19 @@
+import Link from "next/link";
+import "./page.css";
+
 export default function Instructor() {
   return (
     <main>
-      <div className="content">강사 페이지</div>
+      <div className="content">
+        <h1>xxx강사</h1>
+        <div className="line"></div>
+        <nav className="nav">
+          <Link href="/study/instructor/summary">Summary</Link>
+          <Link href="/study/instructor/submission">Submission</Link>
+          <Link href="/study/instructor/student">Student Info</Link>
+        </nav>
+        <div className="line"></div>
+      </div>
     </main>
   );
 }
