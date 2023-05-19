d645acbb9d74a148533f1560e06acf9d4d2b10aa
SeongHyeon Cho <67233988+st42597@users.noreply.github.com>
2023-05-19 18:25:29 +0900

    feat: add footer

--- client/src/app/components/footer.js
+++ client/src/app/components/footer.js
@@ -2,5 +2,28 @@ import Link from "next/link";
 import "./footer.css";
 
 export default function Footer() {
-  return <footer className="footer"></footer>;
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="flex-row">
+          <span className="opacity4">2023 Software Engineering Team4</span>
+          <h2 className="title">Codemy</h2>
+        </div>
+        <div className="line"></div>
+        <div className="flex-row">
+          <div className="flex-row gap">
+            <Link href="/" className="opacity8 text">
+              Home
+            </Link>
+            <Link href="/study" className="opacity8 text">
+              Study
+            </Link>
+          </div>
+          <a href="https://github.com/skkuse/2023spring_41class_team4">
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 }
