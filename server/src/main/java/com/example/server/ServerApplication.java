d5256625079e1872a7a8c4b728a4057a8389adbd
Kim Jun Hyeong <56298375+JunHyeongKim73@users.noreply.github.com>
2023-05-23 20:43:51 +0900

    chore: test commit

--- server/src/main/java/com/example/server/ServerApplication.java
+++ server/src/main/java/com/example/server/ServerApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ServerApplication {
-
+		
+	// hello
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 	}
