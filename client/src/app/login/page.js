0f7eaa23ef9669ac0a2b4b6f88cd2530ce167778
thdwldmszz <63731797+thdwldmszz@users.noreply.github.com>
2023-05-25 05:12:16 +0900

    update src/app/login

--- client/src/app/login/page.js
+++ client/src/app/login/page.js
@@ -1,12 +1,23 @@
 import Link from "next/link";
+import "./page.css";
 
 export default function Login() {
   return (
     <main>
-      로그인 페이지
-      <p>
-        <Link href="/register">아이디가 없는 경우 회원가입 링크</Link>
-      </p>
+      <div class="containerLog">
+        <div class="loginBox">
+          <h3 id="pageTitle">Login</h3>
+          <form class="loginForm" name="loginForm" method="post"  action="https://www.formbackend.com/f/664decaabbf1c319">
+            <input type="text" id="loginEmail" name="loginEmail" placeholder="Email"></input>
+            <input type="password" id="loginPass" name="loginPass" placeholder="Password"></input>
+            <button type="submit" id="btnLogin">로그인</button>
+          </form>
+          <p>
+            아이디가 없는 경우?
+            <Link href="/register" id="signupLink">회원가입</Link>
+          </p>
+        </div>
+      </div>
     </main>
   );
 }
