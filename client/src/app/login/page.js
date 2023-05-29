8db119d8e736b239944093468f45571b1b3f929f
thdwldmszz <63731797+thdwldmszz@users.noreply.github.com>
2023-05-30 06:33:51 +0900

    update login axios

--- client/src/app/login/page.js
+++ client/src/app/login/page.js
@@ -1,16 +1,71 @@
+"use client";
 import Link from "next/link";
 import "./page.css";
+import axios from 'axios';
+
+
+import { useState } from 'react';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [status, setStatus] = useState('');
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+ 
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleStatus = (e) => {
+    setStatus(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (email === '' || password === '' || status == '') {
+      console.log("Lacking data");
+    } else {
+      console.log("done");
+      var url=''
+      if(status == "Teacher"){
+        url='/api/teacher/users/login'
+      }
+      else{
+        url = '/api/users/login'
+      }
+      console.log(url)
+      //redirect('/login');
+        
+      try {
+        const response = await axios.post(url, {
+          "email": email,
+          "password": password
+        });
+        console.log(response);
+      } catch (error) {
+        console.error(error);
+      }
+  }
+  };
+
   return (
     <main>
       <div class="containerLog">
         <div class="loginBox">
           <h3 id="pageTitle">Login</h3>
-          <form class="loginForm" name="loginForm" method="post"  action="https://www.formbackend.com/f/664decaabbf1c319">
-            <input type="text" id="loginEmail" name="loginEmail" placeholder="Email"></input>
-            <input type="password" id="loginPass" name="loginPass" placeholder="Password"></input>
-            <button type="submit" id="btnLogin">로그인</button>
+          <form class="loginForm">
+            <input onChange={handleEmail} className="input" value={email} type="text" placeholder="Email" />
+            <input onChange={handlePassword} className="input" value={password} type="password" placeholder="Password" />
+            <div class="radioForm">
+              <input onChange={handleStatus} type="radio" name="radioType" id="Student" value="Student"></input>
+              <label for="Student">Student</label>
+              <input onChange={handleStatus} type="radio" name="radioType" id="Teacher" value="Teacher"></input>
+              <label for="Teacher">Teacher</label>
+          </div>
+            <button onClick={handleSubmit} id="btnLogin" type="submit">로그인</button>
           </form>
           <p>
             아이디가 없는 경우?
