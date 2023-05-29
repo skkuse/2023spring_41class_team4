0de58e3bde43231684bd1139eece3762cbc74ecc
thdwldmszz <63731797+thdwldmszz@users.noreply.github.com>
2023-05-30 06:32:45 +0900

    update axios

--- client/src/app/register/student/page.js
+++ client/src/app/register/student/page.js
@@ -1,20 +1,71 @@
+"use client";
 import Link from "next/link";
 import "./page.css";
+import axios from 'axios';
+
+import { useState } from 'react';
 
 export default function Register() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [githubAccount, setGithubAccount] = useState('');
+  const [teacherCode, setTeacherCode] = useState('');
+ 
+  const handleName = (e) => {
+      setName(e.target.value);
+    };
+ 
+  const handleEmail = (e) => {
+      setEmail(e.target.value);
+  };
+ 
+  const handlePassword = (e) => {
+      setPassword(e.target.value);
+  };
+
+  const handleGithubAccount = (e) => {
+    setGithubAccount(e.target.value);
+  };
+
+  const handleTeacherCode = (e) => {
+    setTeacherCode(e.target.value);
+  };
+ 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (name === '' || email === '' || password === '' || githubAccount == '' || teacherCode == '') {
+      console.log("Lacking data")
+    } else {
+      console.log("done")
+      //redirect('/login');
+      
+    try {
+      const response = await axios.post('/api/users', {
+        "email": email,
+	      "password": password,
+	      "name": name,
+        "githubAccount": githubAccount,
+	      "teacherCode": teacherCode
+      });
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  };
   return (
     <main>
       <div class="containerLog">
       <div class="signupBox">
-        <h3 id="pageTitle">Sign Up</h3>
-        <form class="signupForm" id="signupForm" name="signupForm" method="post" action="signup.jsp">
-          <input type="email" id="signupEmail" name="signupEmail" class="signupCon" placeholder="Email"></input>
-          <input type="password" id="signupPass" name="signupPass" class="signupCon" placeholder="Password"></input>
-          <input type="password" id="signupPassCon" name="signupPassCon" class="signupCon" placeholder="Confirm password"></input>
-          <input type="text" id="signupName" name="signupName" class="signupCon" placeholder="Name"></input>
-          <input type="text" id="signupGithubId" name="signupGithubId" class="signupCon" placeholder="Github ID"></input>
-          <input type="text" id="signupTeacherId" name="signupTeacherId" class="signupCon" placeholder="Teacher ID"></input>
-          <button type="submit" id="btnSignup">가입하기</button>
+        <h3 id="pageTitle">회원가입</h3>
+        <form class="signupForm">
+          <input onChange={handleName} className="input" value={name} type="text" placeholder="Name" />
+          <input onChange={handleEmail} className="input" value={email} type="text" placeholder="Email" />
+          <input onChange={handlePassword} className="input" value={password} type="password" placeholder="Password" />
+          <input onChange={handleGithubAccount} className="input" value={githubAccount} type="text" placeholder="Github ID" />
+          <input onChange={handleTeacherCode} className="input" value={teacherCode} type="text" placeholder="Teacher ID" />
+          <button onClick={handleSubmit} id="btnSignup" type="submit">가입하기</button>
           <p>
             아이디가 이미 있으면
             <Link href="/login" id="loginLink">로그인</Link>
