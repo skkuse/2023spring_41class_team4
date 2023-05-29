ae6c3194a94ce46e0e8b3810bd76c26a02fef392
SeongHyeon Cho <67233988+st42597@users.noreply.github.com>
2023-05-29 16:41:47 +0900

    fix: fix api cors issue

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination:
+          "https://port-0-codemy-7e6o2clhzvliku.sel4.cloudtype.app/:path*",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- client/src/app/study/instructor/submission/page.js
+++ client/src/app/study/instructor/submission/page.js
@@ -1,9 +1,28 @@
 "use client";
 import Link from "next/link";
+import axios from "axios";
+import { useEffect } from "react";
 import Pagination from "@/app/components/paginate";
 import "./page.css";
 
 export default function Submission() {
+  axios.defaults.withCredentials = true;
+  useEffect(() => {
+    async function getInfo() {
+      const res = await axios.get(
+        "/api/teacher/me",
+        {
+          headers: {
+            "X-Auth-Token": "TEACHER2",
+          },
+        },
+        { withCredentials: true }
+      );
+      console.log(res.data);
+    }
+    console.log("test");
+    getInfo();
+  }, []);
   const data = [];
   for (let i = 1; i < 30; i++) {
     data.push({
