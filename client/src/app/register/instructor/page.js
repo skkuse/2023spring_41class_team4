da367ee21c666e51e3140291413f158a62e5caed
thdwldmszz <63731797+thdwldmszz@users.noreply.github.com>
2023-05-30 06:31:50 +0900

    update register/instructor/axios

--- client/src/app/register/instructor/page.js
+++ client/src/app/register/instructor/page.js
@@ -1,18 +1,65 @@
+"use client";
 import Link from "next/link";
 import "./page.css";
+import axios from 'axios';
 
-export default function Register() {
+
+import { useState } from 'react';
+import { redirect } from 'next/navigation';
+
+
+export default function Register() {  
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+ 
+  const handleName = (e) => {
+      setName(e.target.value);
+    };
+ 
+  const handleEmail = (e) => {
+      setEmail(e.target.value);
+  };
+ 
+  const handlePassword = (e) => {
+      setPassword(e.target.value);
+  };
+ 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (name === '' || email === '' || password === '') {
+      console.log("empty form")
+    } else {
+      console.log("done")
+      //redirect('/login');
+      
+    try {
+      const response = await axios.post('/api/teacher/users', {
+        "email": email,
+	      "password": password,
+	      "name": name
+      });
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  };
+
   return (
     <main>
       <div class="containerLog">
       <div class="signupBox">
-        <h3 id="pageTitle">Sign Up</h3>
-        <form class="signupForm" id="signupForm" name="signupForm" method="post" action="signup.jsp">
-          <input type="email" id="signupEmail" name="signupEmail" class="signupCon" placeholder="Email"></input>
-          <input type="password" id="signupPass" name="signupPass" class="signupCon" placeholder="Password"></input>
-          <input type="password" id="signupPassCon" name="signupPassCon" class="signupCon" placeholder="Confirm password"></input>
-          <input type="text" id="signupName" name="signupName" class="signupCon" placeholder="Name"></input>
-          <button type="submit" id="btnSignup">가입하기</button>
+        <h3 id="pageTitle">회원가입</h3>
+        <form class="signupForm">
+
+                <input onChange={handleName} className="input" value={name} type="text" placeholder="Name" />
+ 
+                <input onChange={handleEmail} className="input" value={email} type="email" placeholder="Email" />
+ 
+                <input onChange={handlePassword} className="input" value={password} type="password" placeholder="Password" />
+ 
+                <button onClick={handleSubmit} id="btnSignup" type="submit">가입하기</button>
           <p>
             아이디가 이미 존재한다면
             <Link href="/login" id="loginLink">로그인</Link>
