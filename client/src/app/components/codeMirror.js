4c71963bd17d98be858ce238fcf3a6fd3dec2401
Choonghyun_Park <choonghyun.park10@gmail.com>
2023-05-30 22:32:16 +0900

    edit codeMirror component

---
+++ client/src/app/components/codeMirror.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import CodeMirror from '@uiw/react-codemirror';
+import { javascript } from '@codemirror/lang-javascript';
+
+function App() {
+  const onChange = React.useCallback((value, viewUpdate) => {
+    console.log('value:', value);
+  }, []);
+  return (
+    <CodeMirror
+      value="console.log('hello world!');"
+      height="200px"
+      extensions={[javascript({ jsx: true })]}
+      onChange={onChange}
+    />
+  );
+}
+export default App;
--- client/src/app/study/student/problem/[id]/page.js
+++ client/src/app/study/student/problem/[id]/page.js
@@ -1,25 +1,72 @@
 "use client"
-import Link from "next/link";
-import "./page.css";
-import CodeEditor from "@/app/components/codeEditor";
-// import { useEffect } from "react";
-// import Prism from "prismjs"
+// import CodeMirror from '@uiw/react-codemirror';
+// import { createTheme } from '@uiw/codemirror-themes';
+// import { javascript } from '@codemirror/lang-javascript';
+// import { tags as t } from '@lezer/highlight';
+// import "./page.css";
+
+
 
 export default function ProblemItem({ params }) {
-  const code = `#include<bits/stdc++.h>
-using namespace std;
+//   const code = `#include<bits/stdc++.h>
+// using namespace std;
   
-int main(){
-  cout << "testing";
+// int main(){
+//   cout << "testing";
 
-  return 0;
+//   return 0;
+// }
+//   `;
+//   return (
+//     <CodeEditor code={code}></CodeEditor>
+
+//   );
+
+  return <>{params.id}</>;
 }
-  `;
-  return (
-    <CodeEditor code={code}></CodeEditor>
 
-  );
+// const myTheme = createTheme({
+//   theme: 'light',
+//   settings: {
+//     background: '#ffffff',
+//     foreground: '#75baff',
+//     caret: '#5d00ff',
+//     selection: '#036dd626',
+//     selectionMatch: '#036dd626',
+//     lineHighlight: '#8a91991a',
+//     gutterBackground: '#fff',
+//     gutterForeground: '#8a919966',
+//   },
+//   styles: [
+//     { tag: t.comment, color: '#787b8099' },
+//     { tag: t.variableName, color: '#0080ff' },
+//     { tag: [t.string, t.special(t.brace)], color: '#5c6166' },
+//     { tag: t.number, color: '#5c6166' },
+//     { tag: t.bool, color: '#5c6166' },
+//     { tag: t.null, color: '#5c6166' },
+//     { tag: t.keyword, color: '#5c6166' },
+//     { tag: t.operator, color: '#5c6166' },
+//     { tag: t.className, color: '#5c6166' },
+//     { tag: t.definition(t.typeName), color: '#5c6166' },
+//     { tag: t.typeName, color: '#5c6166' },
+//     { tag: t.angleBracket, color: '#5c6166' },
+//     { tag: t.tagName, color: '#5c6166' },
+//     { tag: t.attributeName, color: '#5c6166' },
+//   ],
+// });
+// const extensions = [javascript({ jsx: true })];
 
-  // return <>{params.id}</>;
-}
-
+// export default function ProblemItem({ params}) {
+//   const onChange = React.useCallback((value, viewUpdate) => {
+//     console.log('value:', value);
+//   }, []);
+//   return (
+//     <CodeMirror
+//       value="console.log('hello world!');"
+//       height="200px"
+//       theme={myTheme}
+//       extensions={extensions}
+//       onChange={onChange}
+//     />
+//   );
+// }
