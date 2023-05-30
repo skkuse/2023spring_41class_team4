26ae053e379604f8eff29a3622b9178adbd0da85
JunHyeongKim73 <rla5764v@gmail.com>
2023-05-30 21:05:20 +0900

    refactor: 문제 추천 API 예외 처리

--- server/src/main/java/com/example/server/student/problem/ProblemLoaderImpl.java
+++ server/src/main/java/com/example/server/student/problem/ProblemLoaderImpl.java
@@ -2,18 +2,23 @@ package com.example.server.student.problem;
 
 import com.example.server.exceptions.ProblemLoadException;
 import com.example.server.student.problem.dto.SolvedacResponse;
+import java.util.List;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 @Service
-public class ProblemLoaderImpl implements ProblemLoader{
+public class ProblemLoaderImpl implements ProblemLoader {
 
     private static final RestTemplate REST_TEMPLATE = new RestTemplate();
 
     @Override
     public SolvedacResponse load(String bojId) {
+        if (bojId == null || bojId.isEmpty()) {
+            return new SolvedacResponse(0, List.of());
+        }
+
         try {
             return REST_TEMPLATE.exchange(
                     "https://solved.ac/api/v3/user/top_100?handle=" + bojId,
