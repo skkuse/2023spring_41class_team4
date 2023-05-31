d12bf9592702bb6ff925ac262ce8f563fa148d00
thdwldmszz <63731797+thdwldmszz@users.noreply.github.com>
2023-05-31 20:15:30 +0900

    modifed order

--- client/src/app/register/page.js
+++ client/src/app/register/page.js
@@ -9,13 +9,13 @@ export default function Register() {
       <h3 id="pageTitle">ㅇㅇㅇ</h3>
       <div class="signupDiv">
         <div class="classBox">
-        <Link href="/register/instructor"><Image src="/teacher.png"  id="teacherBox" width={100} height={100}/></Link>
-        <p>강사</p>
-        </div>
-        <div class="classBox">
         <Link href="/register/student"><Image src="/student.png" width={100} height={100}/></Link>
         <p>학생</p>
         </div>  
+        <div class="classBox">
+        <Link href="/register/instructor"><Image src="/teacher.png"  id="teacherBox" width={100} height={100}/></Link>
+        <p>강사</p>
+        </div>
       </div>
     </div>
     </main>
