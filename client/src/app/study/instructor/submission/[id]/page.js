c7591ce263df93bae759fbd062f30bd7ccdb009c
SeongHyeon Cho <67233988+st42597@users.noreply.github.com>
2023-05-31 16:07:28 +0900

    feat: add text feedback & submit layout

--- client/src/app/study/instructor/submission/[id]/page.js
+++ client/src/app/study/instructor/submission/[id]/page.js
@@ -1,8 +1,8 @@
 "use client";
 import CodeViwer from "@/app/components/codeViwer";
 import Radar from "@/app/components/radar";
-import "./page.css";
 import { useEffect } from "react";
+import "./page.css";
 
 export default function SubmissionItem({ params }) {
   useEffect(() => {
@@ -17,19 +17,46 @@ int main(){
   return 0;
 }
   `;
+  const feedback = {
+    id: 1,
+    problemId: 1,
+    student: {
+      id: 1,
+      name: "Student1",
+    },
+    createdAt: "2023-05-24 1:20:00",
+    status: "SOLVED",
+    language: "python3",
+    content: "print(a + b)",
+    feedback: {
+      id: 1,
+      overview: "완벽한 코드입니다.",
+      achievement: {
+        efficiency: 100,
+        readability: 79,
+        correctness: 90,
+        scalability: 97,
+        modularity: 89,
+        security: 89,
+      },
+    },
+  };
+
+  const labels = [
+    "efficiency",
+    "readability",
+    "correctness",
+    "scalability",
+    "modularity",
+    "security",
+  ];
+
   const data = {
-    labels: [
-      "Eating",
-      "Drinking",
-      "Sleeping",
-      "Designing",
-      "Coding",
-      "Cycling",
-    ],
+    labels: labels,
     datasets: [
       {
         label: "My First Dataset",
-        data: [65, 90, 81, 56, 55, 80],
+        data: labels.map((x) => feedback.feedback.achievement[x]),
         fill: true,
         backgroundColor: "rgba(255, 99, 132, 0.2)",
         borderColor: "rgb(255, 99, 132)",
@@ -54,8 +81,26 @@ int main(){
           </div>
           <div className="flex-col margin-top">
             <h1>ChatGPT Feedback</h1>
-            <div style={{ width: "300px", height: "300px" }}>
-              <Radar data={data}></Radar>
+            <div className="flex-row">
+              <div style={{ width: "300px", height: "300px" }}>
+                <Radar data={data}></Radar>
+              </div>
+              <div className="feedback-container">
+                {feedback && feedback.feedback.overview}
+              </div>
+            </div>
+          </div>
+          <div className="flex-col margin-top">
+            <h1>Comment</h1>
+            <textarea
+              className="mt10"
+              rows="10"
+              placeholder="comment"
+            ></textarea>
+            <div className="submit-button-container">
+              <button className="submit-button" type="button">
+                Submit
+              </button>
             </div>
           </div>
         </div>
