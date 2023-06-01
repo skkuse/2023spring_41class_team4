54f509eba283dba559c7bdf2fab9bfaf00422809
Choonghyun_Park <choonghyun.park10@gmail.com>
2023-06-01 11:17:58 +0900

    update pagenate_comment

---
+++ client/src/app/components/paginate_comment.js
@@ -0,0 +1,95 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import ReactPaginate from "react-paginate";
+import "./paginate_comment.css";
+
+// Example items, to simulate fetching from another resources.
+
+function Items({ currentItems }) {
+  return (
+    <div style={{ width: "100%", marginTop: "2rem" }}>
+      <header className="submission-header submission-container">
+        <ul>
+          <li>#</li>
+          <li>학생</li>
+          <li>문제</li>
+          <li>제출일</li>
+          <li>상태</li>
+        </ul>
+      </header>
+      <div className="items-container">
+        {currentItems &&
+          currentItems.map((item) => (
+            <Link
+              href={`/study/instructor/submission/${item.id}`}
+              className="item submission-container"
+            >
+              <ul>
+                <li>{item.id}</li>
+                <li>{item.name}</li>
+                <li>{item.problemId}</li>
+                <li>{item.createdAt}</li>
+                <li
+                  style={
+                    item.status == "SOLVED"
+                      ? { color: "#e74c3c" }
+                      : { color: "#009874" }
+                  }
+                >
+                  {item.status}
+                </li>
+              </ul>
+            </Link>
+          ))}
+      </div>
+    </div>
+  );
+}
+
+export default function PaginatedItems({ itemsPerPage, data}) {
+  // Here we use item offsets; we could also use page offsets
+  // following the API or data you're working with.
+  const [itemOffset, setItemOffset] = useState(0);
+
+  // Simulate fetching items from another resources.
+  // (This could be items from props; or items loaded in a local state
+  // from an API endpoint with useEffect and useState)
+  const endOffset = itemOffset + itemsPerPage;
+  console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+  const currentItems = data.slice(itemOffset, endOffset);
+  const pageCount = Math.ceil(data.length / itemsPerPage);
+
+  // Invoke when user click to request another page.
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % data.length;
+    console.log(
+      `User requested page number ${event.selected}, which is offset ${newOffset}`
+    );
+    setItemOffset(newOffset);
+  };
+
+  return (
+    <div id="container">
+      <Items currentItems={currentItems}/>
+      <ReactPaginate
+        className="paginate"
+        breakLabel="..."
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={5}
+        pageCount={pageCount}
+        previousLabel="< previous"
+        renderOnZeroPageCount={null}
+      />
+    </div>
+  );
+}
+
+// Add a <div id="container"> to your HTML to see the component rendered.
+/*
+ReactDOM.render(
+  <PaginatedItems itemsPerPage={4} />,
+  document.getElementById("container")
+);
+*/
--- client/src/app/study/student/comment/page.js
+++ client/src/app/study/student/comment/page.js
@@ -1,20 +1,58 @@
 "use client";
 import Link from "next/link";
-import Pagination from "@/app/components/paginate";
+import axios from "axios";
+import { useState,useEffect } from "react";
+import Pagination from "@/app/components/paginate_comment";
 import "./page.css";
+
+var pageInfo;
+var itemsPerPage;
+var data=[];
+
 export default function Comments() {
-  const data = [];
-  for (let i = 1; i < 30; i++) {
-    data.push({
-      id: i,
-      title: "comment " + i,
-      date: " 2023-05-22",
-    });
-  }
+
+  useEffect(() => {
+    async function getInfo() {
+      const res = await axios.get("/api/submissions", {
+        headers: {
+          "X-Auth-Token": localStorage.Codemy,
+        },
+      });
+      console.log("debug");
+      console.log(res.data);
+      pageInfo = res.data.pageInfo;
+      itemsPerPage = pageInfo.pageSize;
+      // totalPage
+      // currentpage
+      // pageSize
+      // numberOfElements
+      data = [];
+      for (let i = 0; i < pageInfo.numberOfElements; i++) {
+        let submission = res.data.submissions[i];
+        console.log(submission);
+        data.push({
+          id: i+1,
+          name: localStorage.CodemyName,
+          problemId: submission.problemId,
+          createdAt: submission.createdAt,
+          status: submission.status,
+        });
+      }
+    }
+    getInfo();
+  }, []);
+  // const data = [];
+  // for (let i = 1; i < 30; i++) {
+  //   data.push({
+  //     id: i,
+  //     title: "comment " + i,
+  //     date: " 2023-05-22",
+  //   });
+  // }
 
   return (
     <main>
-      <h1>xxx 학생</h1>
+      <h1>{localStorage.CodemyName} 학생</h1>
       <div className="line"></div>      
       <nav className="nav">
         <Link href="/study/student">me</Link>
