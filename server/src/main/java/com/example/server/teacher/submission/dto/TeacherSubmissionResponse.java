5950b925296aaae87efd041c471c280057ed3915
Yeongwoo Kim <yw1221@g.skku.edu>
2023-06-10 17:50:21 +0900

    add student info in teacher submission list

--- server/src/main/java/com/example/server/teacher/submission/dto/TeacherSubmissionResponse.java
+++ server/src/main/java/com/example/server/teacher/submission/dto/TeacherSubmissionResponse.java
@@ -3,6 +3,7 @@ package com.example.server.teacher.submission.dto;
 import com.example.server.student.submission.Comment;
 import com.example.server.student.submission.Submission;
 import com.example.server.student.submission.SubmissionStatus;
+import com.example.server.student.user.Student;
 import com.example.server.teacher.feedback.Achievement;
 import com.example.server.teacher.feedback.Feedback;
 import com.example.server.utils.DateUtils;
@@ -14,6 +15,7 @@ public class TeacherSubmissionResponse {
 
     private Long id;
     private Long problemId;
+    private StudentResponse student;
     private SubmissionStatus status;
     private String language;
     private String content;
@@ -32,8 +34,21 @@ public class TeacherSubmissionResponse {
         this.language = submission.getLanguage();
         this.content = submission.getCode();
         this.createdAt = DateUtils.formattedDate(submission.getCreatedAt());
+        this.student = new StudentResponse(submission.getStudent());
         submission.getFeedback().ifPresent(feedback -> this.feedback = new FeedbackResponse(feedback));
         submission.getComment().ifPresent(comment -> this.comment = new CommentResponse(comment));
+    }
+
+    @Getter
+    private static class StudentResponse {
+
+        private Long id;
+        private String name;
+
+        public StudentResponse(Student student) {
+            this.id = student.getId();
+            this.name = student.getName();
+        }
     }
 
     @Getter
